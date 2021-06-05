9742fab6e6103cf2c9a985571e9bd11b88d0a329
Richard Ng <richardcrng@gmail.com>
2021-06-05 14:57:52 +0100

    Add count occurrences function

---
+++ src/count-occurences.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/**
+ * Count how many whole occurrences of a string there are in an array.
+ * Partial matches are not counted.
+ * The count should be case sensitive.
+ * 
+ * @param {string[]} strArr - array of strings to check
+ * @param {string} targetString - string to count occurrences off
+ * @returns {number} how many occurrences of the string are in the array
+ */
+function countOccurrences(strArr, targetString) {
+  let count = 0;
+  for (let str of strArr) {
+    if (targetString === str) {
+      count++
+    }
+  }
+  return count;
+}
+
+console.log(countOccurrences(["na", "na", "na", "na", "BATMAN"], "na"), 4);
+console.log(countOccurrences(["na", "na", "na", "na", "BATMAN"], "BATMAN"), 1);
+console.log(countOccurrences(["na", "na", "na", "na", "BATMAN"], "batman"), 0);
+console.log(countOccurrences(["na", "na", "na", "na", "BATMAN"], "a"), 0);
