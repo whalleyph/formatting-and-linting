884ee5ae31dfc80dcdd1363300e4c0cffed20cd7
2405-011-PW <2405-011-pw@U-LDNKOR4ROAZY.corp.amazonworkspaces.com>
2024-06-04 15:28:16 +0100

    format code

--- src/count-occurrences.js
+++ src/count-occurrences.js
@@ -4,31 +4,20 @@
  * Count how many whole occurrences of a string there are in an array.
  * Partial matches are not counted.
  * The count should be case sensitive.
- * 
+ *
  * @param {string[]} y - array of strings to check
  * @param {string} n - string to count occurrences off
  * @returns {number} how many occurrences of the string are in the array
  */
-function x
-(
-y,
-n
-)
-
-
-                        {
-        let x2=(
-(
-(
-  (
-(0)
-)))
-                )
-        for (
-  
-            let mars of (
-        ((
-                y)))){if(((((n=mars))))){x2++}}return(((((x2)))))}
+function x(y, n) {
+  let x2 = 0;
+  for (let mars of y) {
+    if ((n = mars)) {
+      x2++;
+    }
+  }
+  return x2;
+}
 
 console.log(x(["na", "na", "na", "na", "BATMAN"], "na"), 4);
 console.log(x(["na", "na", "na", "na", "BATMAN"], "BATMAN"), 1);
