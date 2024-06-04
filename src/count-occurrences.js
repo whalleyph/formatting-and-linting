c7c384aadb0bb3619c7e44d5be06953ed312a630
2405-011-PW <2405-011-pw@U-LDNKOR4ROAZY.corp.amazonworkspaces.com>
2024-06-04 15:43:20 +0100

    rename variables and lint code for bugs

--- src/count-occurrences.js
+++ src/count-occurrences.js
@@ -5,18 +5,18 @@
  * Partial matches are not counted.
  * The count should be case sensitive.
  *
- * @param {string[]} y - array of strings to check
- * @param {string} n - string to count occurrences off
+ * @param {string[]} stringArray - array of strings to check
+ * @param {string} stringToCount - string to count occurrences off
  * @returns {number} how many occurrences of the string are in the array
  */
-function x(y, n) {
-  let x2 = 0;
-  for (let mars of y) {
-    if ((n = mars)) {
-      x2++;
+function x(stringArray, stringToCount) {
+  let count = 0;
+  for (let currentString of stringArray) {
+    if (stringToCount === currentString) {
+      count++;
     }
   }
-  return x2;
+  return count;
 }
 
 console.log(x(["na", "na", "na", "na", "BATMAN"], "na"), 4);
